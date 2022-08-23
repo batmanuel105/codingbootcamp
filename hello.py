542fd36d5bd8b2886f89440ffd2710e6b193cf05
batmanuel <dalekasylum84@gmail.com>
2022-08-22 20:04:29 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,14 @@
+# print("hello wrold")
+x=3 #vriable=value
+# print(x)
+y=3675
+# print(x,y)
+#in python vomments are used to explain the code
+#comments are fir uman insead of computer
+#comments will be ignored by the computer
+#commentsstart with a # and automaicle wnd in the end of a line
+#can put comments aywhere in the file
+x=x+1
+# print (x)
+y=x+1
+print(y)
